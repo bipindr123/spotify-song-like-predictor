de44528554b6e49dcaa2e78ba551c878e43a6499
Bipin D R <bipindr123@gmail.com>
2022-05-10 14:53:46 -0700

    optimization part 2

--- SVM.py
+++ SVM.py
@@ -4,13 +4,44 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import preprocessing
 
 # Import Data
 spotifyData = pd.read_csv('data1.csv')
 
 # Drop label and irrelevant classes
-x = spotifyData.drop(['target','song_title', 'artist', 'serial_num'], axis=1)
+x = spotifyData.drop(['target','song_title', 'serial_num',  'mode', 'key', 'duration_ms'], axis=1)
 y = spotifyData['target']
+
+# min_max_scaler = preprocessing.MinMaxScaler()
+# x_scaled = min_max_scaler.fit_transform(x)
+# x[x.columns.values] = pd.DataFrame(x_scaled)
+
+from sklearn.preprocessing import OneHotEncoder
+#creating instance of one-hot-encoder
+encoder = OneHotEncoder(handle_unknown='ignore')
+
+#perform one-hot encoding on 'team' column 
+encoder_df = pd.DataFrame(encoder.fit_transform(x[['artist']]).toarray())
+
+#merge one-hot encoded columns back with original DataFrame
+x = x.join(encoder_df)
+
+#drop 'artist' column
+x.drop('artist', axis=1, inplace=True)
+
+
+# x["artist"] = x["artist"].astype('category')
+# x["artist"] = x["artist"].cat.codes
+# x.head()
+
+# print(x.dtypes)
+
+#normalizing data
+stddevs = np.std(x,axis=0)+1e-6
+x /= stddevs
+x /= stddevs
+
 
 # Split data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=69)

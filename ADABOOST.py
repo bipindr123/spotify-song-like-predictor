ac0e4b2dd7936564c6c5bffc8a25b5fd3c8371d3
unknown <utkarshneema123@gmail.com>
2022-05-09 20:49:28 -0700

    Some changes in ADABOOST and XGBOOST

--- ADABOOST.py
+++ ADABOOST.py
@@ -10,14 +10,14 @@ spotifyData = pd.read_csv('data1.csv')
 spotifyData = spotifyData.sample(frac=1).reset_index(drop=True)
 
 # Drop label and irrelevant classes
-x = spotifyData.drop(['target','song_title', 'artist'], axis=1)
+x = spotifyData.drop(['target','song_title', 'artist', 'serial_num'], axis=1)
 y = spotifyData['target']
 
 # Split data into training and testing sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
 #create classifier
-abc = AdaBoostClassifier(n_estimators=50, learning_rate=1)
+abc = AdaBoostClassifier(n_estimators=50, learning_rate=0.1)
 
 #fit model
 model = abc.fit(x_train, y_train)
